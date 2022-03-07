047aba6536e76a3dd782a10dd23ff790f392171d
Aurick <aurickserrano@hotmail.com>
2022-03-07 16:33:23 -0300

    cli

--- fatorial.py
+++ fatorial.py
@@ -1,4 +1,5 @@
-from functools import reduce 
+from functools import reduce
+from multiprocessing.sharedctypes import Value 
 from operator import mul
 
 
@@ -6,7 +7,15 @@ def fatorial(numero):
         return reduce (mul, range(1,numero + 1 )) if numero >= 1 else 1
 
 if __name__ ==  '__main__':
-        n = int(input('Forneça um numero inteiro:'))
-        print(fatorial(n))
+        while True:
+                try:
+                        n = int(input('Forneça um numero inteiro:'))
+                except ValueError:
+                        print('ERRO: Forneça um número inteiro positivo.')
+                except KeyboardInterrupt:
+                        print('Execução Finalizada pelo usuario.')
+                        break
+                else:
+                        print(fatorial(n))
 
 
